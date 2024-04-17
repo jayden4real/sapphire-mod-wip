aabe58b9ff5a66784c37f6087ff830dbc789b2eb
Jayden <jayden@jaydens-imac-pro.local>
2024-04-17 12:38:17 -0400

    Initial commit

---
+++ src/main/java/net/jayden/modtesting1002003/TutorialMod.java
@@ -0,0 +1,18 @@
+package net.jayden.modtesting1002003;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.jayden.modtesting1002003.item.ModItems;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "modtesting1002003";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+	}
+}
---
+++ src/main/java/net/jayden/modtesting1002003/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.jayden.modtesting1002003.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.jayden.modtesting1002003.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SAPPHIRE = registerItem("sapphire", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientTabItemGroup(FabricItemGroupEntries entries) {
+        entries.add(SAPPHIRE);
+    }
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientTabItemGroup);
+    }
+}
